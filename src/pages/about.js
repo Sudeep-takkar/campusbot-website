5a3dd3030bb5e0078366535c39ffa4f307f60637
sudeep-takkar <sudeep.takkar@yahoo.com>
2021-07-27 16:48:03 -0400

    initial commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,17 +12,16 @@ const AboutPage = () => {
                 <header className="entry-header">
                   <h1 className="entry-title">About</h1>
                 </header>
-
                 <div className="entry-body">
                   <p>
                     Holly is a free HTML/CSS landing page designed and developed by
                     @pacovitiello and @DavidePacilio!
-                </p>
+                  </p>
                   <p>
                     Holly is suitable for all kind of startups, it's easily
                     customizable, and it's downloadable for personal and commercial
                     use.
-                </p>
+                  </p>
                   <hr />
                   <p>Live demo: <a href="https://demo.cruip.com/holly/">https://demo.cruip.com/holly/</a></p>
                   <hr />
